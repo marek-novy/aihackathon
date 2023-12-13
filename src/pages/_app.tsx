829fd7a4d9e9c17e4d2c070f27d0651a9118979e
Jakub Žaba <jakub.zaba@kiwi.com>
2023-12-13 10:57:11 +0100

    header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
+import React from "react";
 import "../styles/globals.css";
 import { AppProps } from "next/app";
+import OrbitProvider from "@kiwicom/orbit-components/lib/OrbitProvider";
+import { defaultTheme } from "@kiwicom/orbit-design-tokens";
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <OrbitProvider useId={React.useId} theme={{ orbit: defaultTheme }}>
+      <Component {...pageProps} />
+      </OrbitProvider>
+  );
 }
 
 export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <link
+                    href="https://fonts.googleapis.com/css?family=Roboto:400,400i,500,500i,700"
+                    rel="stylesheet"
+                />
                 <script src="https://cdn.tailwindcss.com"></script>
             </Head>
             <body style={{ background: 'rgb(245, 247, 249)' }}>
